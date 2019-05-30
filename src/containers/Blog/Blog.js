2cb69dd0de1ef12c560d2c77f8aca0a83b3035c8
Ivan <machuk.ivan@yandex.by>
2019-05-30 19:12:19 +0300

    Added lazy loading w react suspense

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,40 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from "react";
 // import axios from '../../axios';
-import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 
-import './Blog.css';
-import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+// import NewPost from './NewPost/NewPost';
+
+const NewPost = React.lazy(() => import("./NewPost/NewPost"));
 
 class Blog extends Component {
-    render () {
+    state = {
+        auth: true
+    };
+
+    render() {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink
-                                to="/posts/"
-                                exact
-                                activeClassName="my-active"
-                                activeStyle={{
-                                    color: '#fa923f',
-                                    textDecoration: 'underline'
-                                }}>Posts</NavLink></li>
-                            <li><NavLink to={{
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</NavLink></li>
+                            <li>
+                                <NavLink
+                                    to="/posts/"
+                                    exact
+                                    activeClassName="my-active"
+                                    activeStyle={{
+                                        color: "#fa923f",
+                                        textDecoration: "underline"
+                                    }}
+                                >
+                                    Posts
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to={{
+                                        pathname: "/new-post",
+                                        hash: "#submit",
+                                        search: "?quick-submit=true"
+                                    }}
+                                >
+                                    New Post
+                                </NavLink>
+                            </li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? (
+                        <Route
+                            path="/new-post"
+                            render={() => (
+                                <Suspense fallback={<div>Loading...</div>}>
+                                    <NewPost />
+                                </Suspense>
+                            )}
+                        />
+                    ) : null}
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>Not found</h1>} />
+                    {/* <Redirect from="/" to="/posts" /> */}
                 </Switch>
             </div>
         );
